b641f524098bd5983fd25bdbb146e3a01b4ec294
jordan <jordan@rally.markets>
2024-05-30 15:18:05 -0400

    KAN-20 Removed token refresh if user is in settings and is not a merchant, but recently was successful in signing up with Stripe. (success param)

--- src/layout/AdvisorLayout/index.js
+++ src/layout/AdvisorLayout/index.js
@@ -25,30 +25,13 @@ const AdvisorLayout = () => {
     ) {
       console.log("loading attributes...");
     }
-    // If the ACCOUNT attribute is -1, then the user needs to route to the onboarding page.
+    // If the ACCOUNT attribute is -1 and ADVISOR or RIA is -1, then the user needs to route to the onboarding page.
     else if (
       auth.attributes.ACCOUNT === -1 &&
       (auth.attributes.ADVISOR === -1 || auth.attributes.RIA === -1) &&
       !location.pathname.includes("/onboarding")
     ) {
       navigate("/onboarding");
-    }
-    // If the MERCHANT attribute does not equal 1, the location is /settings, and there's a success query param, refresh tokens.
-    else if (
-      auth.attributes.MERCHANT !== 1 &&
-      location.pathname.includes("/settings") &&
-      !!success
-    ) {
-      const currRefresh = tokenService.getSessionRefreshToken();
-      tokenService
-        .refreshTokenRequest(currRefresh)
-        .then((response) => {
-          console.log("Tokens were refreshed.", response);
-          navigate("/adv/messages");
-        })
-        .catch((error) => {
-          console.log("Tokens were not refreshed.", error);
-        });
     }
   }, [auth.attributes]);
 

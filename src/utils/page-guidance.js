afffab26fc22e239d8f80bedfe6f721584ef0215
jurb17 <jurbaczek17@gmail.com>
2024-06-24 13:38:45 -0400

    KAN-34 Added 'About this Page' text to for each articles page. For public demo version., page guidance.

--- src/utils/page-guidance.js
+++ src/utils/page-guidance.js
@@ -56,6 +56,34 @@ export const pageGuideObject = {
       "Details are limited and custom fields cannot be added until the prospect becomes a client. There are quick links at the top right of the page to manage invoices and communications with this prospect.",
     ],
   },
+  "/adv/articles": {
+    title: "The Articles List Page",
+    description: [
+      "The articles feature is provided to advisors so they can share wisdom on the publicly shared Rally Blog and advertise themselves.",
+      "The concept of a shared advisors blog was formed around the importance of social media and online consuption of advice. The shared blog would allow clients to find the advisors speaking about a topic they're interested in knowing more about.",
+    ],
+  },
+  "/adv/articles/profile": {
+    title: "The Article Overview Page",
+    description: [
+      "This page provides an overview of the article's details, including the category and subcategory, description, and title. The article details can be edited at any time. Updates to the article are typically saved and are public almost instantly. However, articles on this demo version are not public and new articles will not be saved.",
+      "The category and subcategory are used to filter articles on the public blog. The article title and description are presented on each display card in the public list of articles.",
+    ],
+  },
+  "/adv/articles/new": {
+    title: "The New Article Page",
+    description: [
+      "This page provides advisors with an opportunity to write about their practice and advertise themselves. The title and body of the article function like a blog post. The Rally application is meant to process the article into a new public-facing blog post on the rally.markets website for the world to see.",
+      "In the next page, the advisor will be asked to provide more details about the article before publishing.",
+    ],
+  },
+  "/adv/articles/preview": {
+    title: "The Article Preview Page",
+    description: [
+      "This page is meant to gather the final details about the article before publishing. The category and subcategory are selected by the advisor so their article can be discovered by folks looking for insight about a specific topic. The public blog posts can be filtered by category and subcategory.",
+      "On the public blog, articles are listed on cards. The description text here will be presented on the article card to give the user an idea about the content of the article before selecting it.",
+    ],
+  },
   // CLIENT PAGES
   "/client/messages": {
     title: "The Messages Page",
